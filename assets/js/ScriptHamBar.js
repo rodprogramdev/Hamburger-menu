885407087c9151e0d0aa4baf48938fd56969186e
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2022-08-27 21:30:18 +0800

    STEP19: add hamburger eventListener

--- assets/js/ScriptHamBar.js
+++ assets/js/ScriptHamBar.js
@@ -1,2 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav");
+
+//When we click the hamburger we want to activate the class to create x and show menu
+hamburger.addEventListener("click", () =>{
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
